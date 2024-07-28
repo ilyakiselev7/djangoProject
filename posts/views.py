42d919f030f32547f7ea99a7a527081776b235a7
ilyakiselev7 <nikolaich.ne@gmail.com>
2024-07-29 00:28:36 +0300

    add blog app

--- posts/views.py
+++ posts/views.py
@@ -6,5 +6,5 @@ from .models import Post
 # Create your views here.
 class HomePageView(ListView):
     model = Post
-    template_name = 'home.html'
+#    template_name = 'home.html'
     context_object_name = 'all_posts_list'
